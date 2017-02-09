ea83f47f37e87af18c8b97f1427eae33a7d2d29a
Torsten Schmidt <ichbinschmidt@googlemail.com>
2017-02-09 21:31:04 +0100

    Refactoring

--- Datengenerator/Datengenerator/Kern/FeldHash.cs
+++ Datengenerator/Datengenerator/Kern/FeldHash.cs
@@ -19,9 +19,7 @@ namespace Datengenerator.Kern
 
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i < hash.Length; i++)
-            {
                 sb.Append(hash[i].ToString("X2"));
-            }
 
             return sb.ToString();
         }
