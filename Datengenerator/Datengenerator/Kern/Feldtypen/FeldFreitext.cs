06078013fcb96e25a301f9fcb5b1b2f62d7ff412
Torsten Schmidt <ichbinschmidt@googlemail.com>
2017-09-18 20:18:54 +0200

    Zufällige statt konstante Textlänge für variable Textfelder. Closes #91.

--- Datengenerator/Datengenerator/Kern/Feldtypen/FeldFreitext.cs
+++ Datengenerator/Datengenerator/Kern/Feldtypen/FeldFreitext.cs
@@ -9,6 +9,7 @@ namespace Datengenerator.Kern
     class FeldFreitext : Feld
     {
         private int stellen;
+        private bool variableLänge;
         private const string blindtext = "Zwei flinke Boxer jagen die quirlige Eva und ihren Mops durch Sylt. Zwölf Boxkämpfer jagen Viktor quer über den großen Sylter Deich. Vogel Quax zwickt Johnys Pferd Bim. Sylvia wagt quick den Jux bei Pforzheim. Polyfon zwitschernd aßen Mäxchens Vögel Rüben, Joghurt und Quark. Fix, Schwyz!, quäkt Jürgen blöd vom Paß. Victor jagt zwölf Boxkämpfer quer über den großen Sylter Deich. Falsches Üben von Xylophonmusik quält jeden größeren Zwerg. Heizölrückstoßabdämpfung. Franz jagt im komplett verwahrlosten Taxi quer durch Bayern.";
 
         public string Dateiattribut;
@@ -16,6 +17,7 @@ namespace Datengenerator.Kern
 
         public FeldFreitext(XElement xml, Random r, Dictionary<string, string> dateiattribute) : base(xml, r)
         {
+            variableLänge = xml.Element("Stellen").Value.Contains("<=");
             stellen = int.Parse(xml.Element("Stellen").Value.Replace("<=", ""));
 
             Dateiattribute = dateiattribute;
@@ -37,6 +39,8 @@ namespace Datengenerator.Kern
 
                 if (Dateiattribut != null)
                     return Dateiattribute[Dateiattribut];
+                else if (variableLänge)
+                    return blindtext.Substring(0, Random.Next(1, stellen));
                 else
                     return blindtext.Substring(0, stellen);
             }
